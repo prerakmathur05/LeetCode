bbff0b91a2e1d422abf345cb7a24571231e30818
Prerak Mathur <prerakrk7@gmail.com>
2022-01-07 16:44:30 -0800

    Time: 65 ms (6.24%), Space: 14.2 MB (47.34%) - LeetHub

--- 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.py
+++ 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.py
@@ -1,19 +1,18 @@
 class Solution:
     def maxAscendingSum(self, nums: List[int]) -> int:
-        result=0
-        curr=float("-inf")
-        maxi=0
+        #WE will  use Kadane's Algorithm for this problem
+        curr=-1
+        result=final_result=0
         for value in nums:
             if value > curr:
                 curr=value
                 result+=curr
             else:
-                maxi=max(maxi,result)
+                final_result=max(final_result,result)
                 curr=value
-                result=value
-                
-        maxi=max(maxi,result)
-        return maxi
+                result=value       
+        final_result=max(final_result,result)
+        return final_result
             
             
                 
