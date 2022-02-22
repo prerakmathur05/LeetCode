6a7fc4f56b02b1c8c3c6ea0642294eac96ab54f4
Prerak Mathur <prerakrk7@gmail.com>
2022-02-22 13:34:46 -0800

    Time: 70 ms (72.60%), Space: 13.9 MB (84.65%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,14 +1,14 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         num=str(x)
-        def ispalin(s,left,right):
-            if left>=right:
-                return True
-            if s[left]!=s[right]:
+        r=len(num)-1
+        l=0
+        while l<r:
+            if num[l]==num[r]:
+                l+=1
+                r-=1
+            else:
                 return False
-            rec=ispalin(s,left+1,right-1)
-            return True if rec else False
+        return True
         
-        return ispalin(num,0,len(num)-1)
-        
-        
+            
