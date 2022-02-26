396fb4453b5a1b2ba19f8b75a83a748b44cfaebe
Prerak Mathur <prerakrk7@gmail.com>
2022-02-26 15:51:23 -0800

    Time: 8435 ms (5.01%), Space: 32.4 MB (5.21%) - LeetHub

---
+++ 55-jump-game/55-jump-game.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n=len(nums)
+        @lru_cache(maxsize=None)
+        def dp(i):
+            if i==n-1:
+                return True
+            if nums[i]==0:
+                return False
+            farjump=min(n-1-i,nums[i])
+            for j in range(farjump,0,-1):
+                c=dp(i+j)
+                if c:
+                    return True
+            return False
+        return dp(0)
+            
+            
