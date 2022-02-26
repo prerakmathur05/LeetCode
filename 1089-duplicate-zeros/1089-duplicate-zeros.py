7ec6081b6430f9e62a1b759cc597cbf45ee0d0aa
Prerak Mathur <prerakrk7@gmail.com>
2022-02-26 10:34:39 -0800

    Time: 94 ms (61.92%), Space: 14.8 MB (65.28%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.py
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -4,17 +4,17 @@ class Solution:
         Do not return anything, modify arr in-place instead.
         """
         n=len(arr)
-        i=0
-        while i<n:
-            if arr[i]==0:
-                j=n-2
-                while j>i:
-                    arr[j+1]=arr[j]
-                    j-=1
-                if i<n-1:
-                    arr[i+1]=0
-                i+=1
-            i+=1
-        return arr
-            
+        r=[]
+        for num in arr:
+            if num==0:
+                r.append(0)
+                r.append(0)
+            else:
+                r.append(num)
+        for i in range(n):
+            arr[i]=r[i]
+        
+
+                
+        
         
